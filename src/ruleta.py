c8c4f0c287c6a30251e8f0642b38250e5313280b
Ivan <iv.morenoma@gmail.com>
2024-02-14 16:54:09 +0100

    initial commit

---
+++ src/ruleta.py
@@ -0,0 +1,12 @@
+def surprise_to(name):
+    print(name)
+
+def main():
+
+   
+    input_name = input("please insert a name") 
+    surprise_to(input_name)
+    
+
+if __name__ == "__main__":
+   main()
